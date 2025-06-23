0cfc66db333241983d46ca22e007e6e98fd1f926
ELMACHHOUNE <mohamed20rida@gmail.com>
2025-06-23 16:00:12 +0100

    Refactor form layout in form.blade.php by removing unnecessary shadow class from image container and correcting gradient class for text overlay.

--- resources/views/layout/form.blade.php
+++ resources/views/layout/form.blade.php
@@ -25,7 +25,7 @@
           >
             <!-- Left side: Image and Text -->
             <div
-              class="order-2 lg:order-1 lg:col-span-2 relative flex justify-center items-center rounded-xl shadow-2xl overflow-hidden h-full"
+              class="order-2 lg:order-1 lg:col-span-2 relative flex justify-center items-center rounded-xl  overflow-hidden h-full"
             >
               <img
                 src="./assets/assurance_retraite.png"
@@ -35,7 +35,7 @@
 
               <!-- Text content overlay on image -->
               <div
-                class="absolute inset-0 flex flex-col justify-end items-center text-white text-center p-8 bg-gradient-to-t from-black/70 to-transparent"
+                class="absolute inset-0 flex flex-col justify-end items-center text-white text-center p-8 from-black/70 to-transparent"
               >
                 <div class="inline-block p-2 rounded-lg bg-black/5"></div>
               </div>
